90acdd38a96e3d1f5c6f0c6ea5ee8b67815b7065
kleber.barilli <kleber.barilli73@gmail.com>
2023-04-26 16:35:54 -0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ConvertResponse {
+  success: boolean;
+  song_title?: string;
+  song_link?: string;
+  message?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'easy-converter';
+  videoUrl: string = '';
+  success: boolean | undefined;
+  songTitle: string | undefined;
+  songLink: string | undefined;
+  errorMessage: string | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  convertVideo() {
+    this.success = undefined;
+    this.errorMessage = undefined;
+
+    this.http
+      .post<ConvertResponse>('/convert-mp3', { videoUrl: this.videoUrl })
+      .subscribe(
+        (response) => {
+          this.success = response.success;
+
+          if (response.success) {
+            this.songTitle = response.song_title;
+            this.songLink = response.song_link;
+          } else {
+            this.errorMessage = response.message;
+          }
+        },
+        (error) => {
+          console.log('Error converting video', error);
+          this.success = false;
+          this.errorMessage =
+            'An error occurred while converting the video. Please try again later.';
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
